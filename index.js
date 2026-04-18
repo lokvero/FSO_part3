fc099573925bc205a6448a581cae24fd0c3258d2
carlos <lokvero@gmail.com>
2026-04-18 22:00:24 +0200

    changes for 3.10

--- index.js
+++ index.js
@@ -104,6 +104,10 @@ app.post('/api/persons',(request, response)=>{
     return response.status(201).json(person)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
