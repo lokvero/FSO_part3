ffd90c9800a5d55347b4b14c624a8d283f85426a
carlos <lokvero@gmail.com>
2026-04-20 17:16:18 +0200

    Exercise 3.15 Add delete, frontend ok

--- index.js
+++ index.js
@@ -38,8 +38,17 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    response.status(500).end()
   })
 })
 
@@ -55,30 +64,27 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/favicon.svg', (req, res) => res.status(204).end()); // Error GET favicon.svg not found
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons',(request, response)=>{
     const body = request.body
-    if ( (!body.name)||(!body.number) ){
-        return response.status(400).json({error:"Missing name or number"})
-    }
-    else if (persons.filter( person => person.name === body.name ).length){
-        return response.status(500).json({error:"Name must be unique"})
-    }
 
     const person = new Person({
       name: body.name,
       number: body.number,
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error=>next(error))
 })
 
 const unknownEndpoint = (request, response) => {
